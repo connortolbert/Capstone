b6306632461edf81075a1825a4421f20ac9c729b
connortolbert <71449340+connortolbert@users.noreply.github.com>
2021-11-08 14:31:03 -0600

    Add files via upload

--- enc_test.py
+++ enc_test.py
@@ -1,6 +1,12 @@
-import motors as mtrs
-import time
+import L2_Speed_Control as sc
+import L2_Inverse_Kinematics as inv
+import encoders as enc
 
 while 1:
-    mtrs.Test()
-    time.sleep(10)
+    myVelocities = np.array([0.4, 0])  # input your first pair
+    myPhiDots = inv.convert(myVelocities)
+    encValues = enc.readShaftPositions()
+    print("Left: ", encValues[0], "\t", "Right: ", encValues[1])
+    for i in range(4):
+        while(encValues[0] != (enc.readShaftPositions()[0])):
+            sc.driveOpenLoop(myPhiDots)
